03f9c1e7772035531b658a429c117bc78fd7e7a3
Nico Cobb <nicocobb@gmail.com>
2024-04-30 22:33:22 -0400

    begin adding new windows

---
+++ src/AboutMe.js
@@ -0,0 +1,9 @@
+// AboutMe.js
+
+import React from 'react';
+
+function AboutMe() {
+  return <h2>About Me</h2>;
+}
+
+export default AboutMe;
---
+++ src/Projects.js
@@ -0,0 +1,9 @@
+// Projects.js
+
+import React from 'react';
+
+function Projects() {
+  return <h2>Projects</h2>;
+}
+
+export default Projects;
