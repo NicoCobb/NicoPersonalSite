18e777acffd493dea14e863c93f36470299a796a
Nico Cobb <nicocobb@gmail.com>
2024-05-02 23:14:30 -0400

    first draft about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -3,7 +3,25 @@
 import React from 'react';
 
 const AboutMe = ()=> {
-  return <h2>About Me</h2>;
+  return (
+    <div>
+        <h2>About Nico Cobb</h2>
+        <p>I am a Software developer who has been 
+            in the industry for about two years. 
+            In my own time, I work on game projects primarily
+            and recently have been venturing out further into various
+            branches of software development.
+            In my own time I have worked on AWS based projects, am currently working on
+            a custom game engine, and various multi-discipline projects
+            in my games. I have worked on the Godot engine, gaining experience
+            working in FOSS Systems.
+
+            In my professional career, I have worked primarily in web based
+            projects. These have been built using React and Angular webstacks.
+            I have worked on team sizes ranging from three developers up to ten.
+        </p>
+    </div>
+  );
 }
 
 export default AboutMe;
