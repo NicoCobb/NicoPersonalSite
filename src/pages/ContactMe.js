ab5469f9db87f53280e494741ece1cb8305fdcba
NICO COBB <nicocobb@gmail.com>
2024-05-16 04:48:44 -0400

    fill out basic contact me page

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,7 +1,22 @@
 // ContactMe.js
 
+import { Col, Container, Row } from "react-bootstrap";
+
 const Contact = () => {
-    return <h2>Contact Me</h2>;
+    return(
+      <Container>
+      <h2>Contact Me</h2>
+      <Row sm={1} md={1} lg={1}>
+      <Col>
+        <a href="https://github.com/NicoCobb">Github</a>
+      </Col>
+      <Col>
+        <a href="https://www.linkedin.com/in/nicolas-cobb-7b714a122/">LinkedIn</a>
+      </Col>
+      </Row>
+      </Container>
+    );
+
   }
   
   export default Contact;
