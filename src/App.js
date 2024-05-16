85b850cad89304c9e7c621660f7387f4b2445f71
NICO COBB <nicocobb@gmail.com>
2024-05-15 22:37:55 -0400

    remove extra navbar and adjust coloring

--- src/App.js
+++ src/App.js
@@ -9,30 +9,20 @@ import Contact from './pages/ContactMe';
 import { Container } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
-import { NavDropdown } from 'react-bootstrap';
 
 const App = ()=> {
   return (
     <Router>
-      <Navbar expand="lg" className='bg-body-tertiary'>
+      <Navbar expand="lg" className='bg-body-tertiary' data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">Nico's Site</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href='/projects'>Projects</Nav.Link>
+            <Nav.Link href='/contact'>Contact Me</Nav.Link>
+            <Nav.Link href='/about'>About Me</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,8 @@
 // Home.js
 
-import Layout from "./Layout";
-import { Navbar } from "react-bootstrap";
-import {Container} from "react-bootstrap";
-import { Nav } from "react-bootstrap";
-
 const Home = () => {
   return (
   <div>
-  <Layout/>
-  <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#about">About Me</Nav.Link>
-            <Nav.Link href="#contact">Contact Me</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
   <h2>Home</h2>
   </div>);
 }
